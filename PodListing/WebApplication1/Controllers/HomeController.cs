fba9c76e62a43643c01f7638e15e765c4cdcebdd
IngeborgAsplund <ingeborg.m.asplund@gmail.com>
2023-05-02 12:33:39 +0200

    MVC controller implemented
    Moved caller logic to an MVC Controller now information can be parsed succesfully. Frontend is not yet finisihed

---
+++ PodListing/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Podlisting_helper;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BroadcastProcessing processor;
+        private int P1Id = 132;
+        private int P2Id = 163;
+        private int P3Id = 164;
+        public List<BroadCastModel> P1Humor { get; set; }
+        public List<BroadCastModel> P2Humor { get; set; }
+        public List<BroadCastModel> P3Humor { get; set; }
+        public async Task<IActionResult> Index()
+        {
+            await LoadProgramInformation();
+            return View();
+        }
+        private async Task LoadProgramInformation()
+        {
+            processor = new BroadcastProcessing();
+
+            P1Humor = await processor.GetHumorPods(P1Id);
+            P2Humor = await processor.GetHumorPods(P2Id);
+            P3Humor = await processor.GetHumorPods(P3Id);
+
+        }
+    }
+}
--- PodListing/WebApplication1/Pages/Index.cshtml.cs
+++ PodListing/WebApplication1/Pages/Index.cshtml.cs
@@ -6,13 +6,7 @@ namespace WebApplication1.Pages
 {
     public class IndexModel : PageModel
     {
-        private BroadcastProcessing processor;
-        private int P1Id = 132;
-        private int P2Id = 163;
-        private int P3Id = 164;
-        public List<BroadCastModel> P1Humor { get; set; }
-        public List<BroadCastModel> P2Humor { get; set; }
-        public List<BroadCastModel> P3Humor { get; set; }
+       
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -21,19 +15,11 @@ namespace WebApplication1.Pages
             
         }
 
-        public async Task OnGet()
+        public void OnGet()
         {
-            await LoadProgramInformation();
-        }
-
-        private async Task LoadProgramInformation() 
-        {
-            processor = new BroadcastProcessing();
-
-            P1Humor = await processor.GetHumorPods(P1Id);
-            P2Humor = await processor.GetHumorPods(P2Id);
-            P3Humor = await processor.GetHumorPods(P3Id);
             
         }
+
+      
     }
 }
